3272a0b75bb60c9713ceb296be3786f97d636162
eahenle <eahenle@gmail.com>
2022-09-30 15:22:07 -0700

    is_isomorphic

--- test/graph_conversion.jl
+++ test/graph_conversion.jl
@@ -1,31 +1,5 @@
 using Graphs, MetaGraphs, MolecularGraph, MolecularGraphKernels, Test
 
-include("check_isom.jl")
-
-@testset "is_isomporphic" begin
-    A = MetaGraph(smilestomol("C1CC=1"))
-    
-    # trivial test (identical graphs)
-    B = deepcopy(A)
-    @test is_isomorphic(A, B)
-
-    # less trivial test (isomorphic but not identical)
-    B = deepcopy(A)
-    set_prop!(B, 3, 1, :label, 1)
-    set_prop!(B, 1, 2, :label, 2)
-    @test is_isomorphic(A, B)
-    
-    # isomorphism broken by changing atom identity
-    B = deepcopy(A)
-    set_prop!(B, 2, :label, 7)
-    @test !is_isomorphic(A, B)
-
-    # isomorphism broken by changing bond order
-    B = deepcopy(A)
-    set_prop!(B, 3, 1, :label, 1)
-    @test !is_isomorphic(A, B)
-end
-
 @testset "graph_conversion" begin
     # test that graph converted from GraphMol equivalent to manually generated
     A = MetaGraph(smilestomol("C1CC=1"))
