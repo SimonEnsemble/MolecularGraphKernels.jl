16a5192aaad39bd01a7a7d089befa3a724b6c870
eahenle <eahenle@gmail.com>
2022-10-03 00:16:17 -0700

    test coverage

--- test/misc.jl
+++ test/misc.jl
@@ -16,8 +16,18 @@ using IOCapture, MolecularGraphKernels, Test
     @testset "isequal" begin
         g₁ = MetaGraph(smilestomol("CN(CC(=O)O)C(=N)N"))
         g₂ = MetaGraph(smilestomol("C(CCN)C[C@@H](C(=O)O)N"))
-        @test isequal(ProductGraph{Direct}(g₁, g₂), ProductGraph{Direct}(ProductGraph{Factor}(g₁, g₂)))
+        dpg = ProductGraph{Direct}(g₁, g₂)
+        @test isequal(dpg, ProductGraph{Direct}(ProductGraph{Factor}(g₁, g₂)))
         g₃ = MetaGraph(smilestomol("c1ccccc1"))
-        @test !isequal(ProductGraph{Direct}(g₁, g₂), ProductGraph{Direct}(g₂, g₃))
+        @test !isequal(dpg, ProductGraph{Direct}(g₂, g₃))
+        dpg2 = deepcopy(dpg)
+        set_prop!(dpg2.graph, 2, :label, 15)
+        @test !isequal(dpg, dpg2)
+        dpg2 = deepcopy(dpg)
+        @assert add_edge!(dpg2.graph, 6, 9, Dict(:label => 1))
+        @test !isequal(dpg, dpg2)
+        dpg2 = deepcopy(dpg)
+        set_prop!(dpg2.graph, first(edges(dpg2.graph)), :label, 0)
+        @test !isequal(dpg, dpg2)
     end
 end
