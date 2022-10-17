0449eeccb3146152f287d363062872a1cf3bb60b
Adrian Henle <eahenle@gmail.com>
2022-10-17 11:24:20 -0700

    isomorphic subgraphs (#53)

--- test/misc.jl
+++ test/misc.jl
@@ -4,7 +4,7 @@ using IOCapture, MolecularGraphKernels, Test
     @testset "display" begin
         g₁ = smilestomol("c1ccccc1")
         g₂ = smilestomol("c1ncccc1")
-        dpg = ProductGraph{Modular}(g₁, g₂)
+        dpg = ProductGraph{Direct}(g₁, g₂)
 
         captured_text = IOCapture.capture() do
             display(dpg)
@@ -12,5 +12,32 @@ using IOCapture, MolecularGraphKernels, Test
         end.output
 
         @test contains(captured_text, "NODES") && contains(captured_text, "EDGES")
+    end
+
+    @testset "MPG isomorphic subgraphs" begin
+        g₁ = smilestomol("NC=O")
+        g₂ = smilestomol("CN(C=O)C=O")
+        mpg = ProductGraph{Modular}(g₁, g₂)
+        imsgs = isomorphic_subgraphs(mpg; min_size=2)
+
+        manual_isom1 = MetaGraph(3)
+        set_prop!(manual_isom1, 1, :label, 7)
+        set_prop!(manual_isom1, 2, :label, 6)
+        set_prop!(manual_isom1, 3, :label, 8)
+        add_edge!(manual_isom1, 1, 2, Dict(:label => 1))
+        add_edge!(manual_isom1, 1, 3, Dict(:label => 0))
+        add_edge!(manual_isom1, 3, 2, Dict(:label => 2))
+        manual_isom1 = ProductGraph{Modular}(manual_isom1)
+
+        manual_isom3 = MetaGraph(2)
+        set_prop!(manual_isom3, 1, :label, 6)
+        set_prop!(manual_isom3, 2, :label, 7)
+        add_edge!(manual_isom3, 1, 2, Dict(:label => 1))
+        manual_isom3 = ProductGraph{Modular}(manual_isom3)
+
+        @test length(imsgs) == 3
+        @test is_isomorphic(imsgs[1], manual_isom1)
+        @test is_isomorphic(imsgs[2], manual_isom1)
+        @test is_isomorphic(imsgs[3], manual_isom3)
     end
 end
