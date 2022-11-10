49f3a1aa3b6dc83fdfebbff988a8e2fd1f2b06da
Cory Simon <cokes@Corys-MacBook-Pro.local>
2022-11-09 17:44:32 -0800

    should be 1 matching subgraph

--- test/graph_kernels.jl
+++ test/graph_kernels.jl
@@ -13,6 +13,21 @@ using MolecularGraph, MolecularGraphKernels, Test
         @test x == random_walk(g₁, g₂; l=l)
     end
 
+    @testset "ethanol-acetic acid test (EZ to count)" begin
+        # the two input molecular graphs
+        g₁ = MetaGraph(smilestomol("CC(O)=O")) # acetic acid
+        g₂ = MetaGraph(smilestomol("CCO")) # ethanol
+
+        ## random walks
+        # l = 1. C <--> C * 4 + O <--> O * 2
+        @test random_walk(g₁, g₂, l=1) == 6
+        # l = 2. C - C <--> C -- C * 4 + C - O <--> C - O * 2 + O - C <--> O - C * 2
+        @test random_walk(g₁, g₂, l=2) == 8
+
+        ## matching connected subgraphs
+        @test subgraph_matching(g₁, g₂) == 1
+    end
+
     @testset "Common Subgraph Isomorphism" begin
         x = common_subgraph_isomorphism(g₁, g₂)
         @test x == 15
