729a60865843439a6d342e550f3894ff2bc43905
Adrian Henle <eahenle@gmail.com>
2022-11-13 17:41:45 -0800

    tests

--- test/graph_kernels.jl
+++ test/graph_kernels.jl
@@ -3,9 +3,8 @@ module Test_graph_kernels
 using MolecularGraph, MolecularGraphKernels, Test
 
 @testset verbose = true "Graph Kernels" begin
-    g₁, g₂ = smilestomol.(["NC=O", "CN(C=O)C=O"])
-
     @testset "Random Walk" begin
+        g₁, g₂ = smilestomol.(["NC=O", "CN(C=O)C=O"])
         l = 4
         x = random_walk(product_graph_adjacency_matrix(Direct, g₁, g₂); l=l)
         @test x == 74
@@ -21,21 +20,21 @@ using MolecularGraph, MolecularGraphKernels, Test
         ## random walks
         # l = 1. C <--> C * 4 + O <--> O * 2
         @test random_walk(g₁, g₂; l=1) == 6
-        # l = 2. C - C <--> C -- C * 4 + C - O <--> C - O * 2 + O - C <--> O - C * 2
+        # l = 2. C - C <--> C - C * 4 + C - O <--> C - O * 2 + O - C <--> O - C * 2
         @test random_walk(g₁, g₂; l=2) == 8
 
         ## matching connected subgraphs
-        @test subgraph_matching(g₁, g₂) == 1
-    end
-
-    @testset "Common Subgraph Isomorphism" begin
-        x = common_subgraph_isomorphism(g₁, g₂)
-        @test x == 15
-        @test x == common_subgraph_isomorphism(ProductGraph{Modular}(g₁, g₂))
-        @test common_subgraph_isomorphism(g₁, g₂; λ=length) == 26
+        # C ↔ C x4
+        # O ↔ O x2
+        # C-C ↔ C-C x2
+        # C-O ↔ C-O x1
+        # C-C-O ↔ C-C-O x1
+        @test ccsi(g₁, g₂) == 10
+        @test ccsi(g₁, g₂; λ=length) == 15
     end
 
     @testset "Connected Common Subgraph Isomorphism" begin
+        g₁, g₂ = smilestomol.(["NC=O", "CN(C=O)C=O"])
         mpg = ProductGraph{Modular}(g₁, g₂)
         @test ccsi(mpg) == 13
         @test ccsi(mpg; λ=length) == 22
