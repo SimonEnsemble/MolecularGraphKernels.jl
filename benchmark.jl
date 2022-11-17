ceb9a7faf18ff6110d09c8b31153d6d2b6e60370
Adrian Henle <eahenle@gmail.com>
2022-11-17 15:20:51 -0800

    correction

--- benchmark.jl
+++ benchmark.jl
@@ -1,4 +1,9 @@
-using BenchmarkTools, MolecularGraph, MolecularGraphKernels
+using BenchmarkTools, IOCapture, MolecularGraph
+import Pkg
+IOCapture.capture() do
+    Pkg.activate(".")
+end
+using MolecularGraphKernels
 
 g₁, g₂ = MetaGraph.(
     smilestomol.(
@@ -9,4 +14,35 @@ g₁, g₂ = MetaGraph.(
     )
 )
 
-@btime ccsi(g₁, g₂)
+capture = IOCapture.capture() do 
+    return eval(quote @btime ccsi(g₁, g₂) end)
+end
+
+k = capture.value
+t = capture.output
+
+@info "CCSI on Two Graphs: $t"
+if k ≠ 6205
+    @error "k = $k"
+end
+
+capture = IOCapture.capture() do
+    return eval(quote @btime ProductGraph{Modular}(g₁, g₂) end)
+end
+
+mpg = capture.value
+t = capture.output
+
+@info "Product Graph: $t"
+
+capture = IOCapture.capture() do 
+    return eval(quote @btime ccsi(mpg) end)
+end
+
+k = capture.value
+t = capture.output
+
+@info "Kernel: $t"
+if k ≠ 6205
+    @error "k = $k"
+end
--- src/kernels/connected_csi.jl
+++ src/kernels/connected_csi.jl
@@ -11,16 +11,15 @@ function ccsi(M::AbstractMatrix; λ::Function=_ -> 1)::Int
     P  = falses(n)
     D  = falses(n)
     S  = falses(n)
-    Cₒ = falses(n)
+    Cₒ = [0]
     # algorithm registers (recursive)
-    N  = falses(n)
+    N = falses(n)
     P′ = falses(n)
     D′ = falses(n)
     S′ = falses(n)
-    C′ = falses(n)
 
-    function bkc_core(C::U, P::U, D::U, S::U) where {U <: BitVector}
-        value += λ(findall(C))
+    function bkc_core(C::Vector{Int}, P::U, D::U, S::U) where {U <: BitVector}
+        value += λ(C)
         if !any(P) && !any(S)
             return
         else
@@ -29,7 +28,6 @@ function ccsi(M::AbstractMatrix; λ::Function=_ -> 1)::Int
                 P′ .= P
                 D′ .= D
                 S′ .= S
-                C′ .= C
                 N .= false
                 @inbounds for v in findall(M[:, uᵢ] .≠ 0)
                     N[v] = true
@@ -44,11 +42,7 @@ function ccsi(M::AbstractMatrix; λ::Function=_ -> 1)::Int
                         @inbounds D′[v] = false
                     end
                 end
-                @inbounds C′[uᵢ] = true
-                P′ .= P′ .& N
-                D′ .= D′ .& N
-                S′ .= S′ .& N
-                bkc_core(C′, P′, D′, S′)
+                bkc_core(C ∪ [uᵢ], P′ .& N, D′ .& N, S′ .& N)
                 @inbounds S[uᵢ] = true
             end
         end
@@ -58,7 +52,6 @@ function ccsi(M::AbstractMatrix; λ::Function=_ -> 1)::Int
         P .= false
         D .= false
         S .= false
-        Cₒ .= false
         @inbounds for v in findall(M[:, u] .≠ 0)
             @inbounds if M[u, v] ≠ D_EDGE
                 @inbounds if T[v]
@@ -70,7 +63,7 @@ function ccsi(M::AbstractMatrix; λ::Function=_ -> 1)::Int
                 @inbounds D[v] = true
             end
         end
-        @inbounds Cₒ[u] = true
+        @inbounds Cₒ[1] = u
         bkc_core(Cₒ, P, D, S)
         @inbounds T[u] = true
     end
