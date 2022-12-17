99d13549ddf4ef132e0810595443d497c69d36cc
Adrian Henle <eahenle@gmail.com>
2022-12-17 14:58:02 -0800

    refactor parallelism (#88)
    * refactor parallelism
    
    * format
    
    * format
    
    * JuliaFormatter Action Bot (#89)
    
    Co-authored-by: eahenle <eahenle@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: eahenle <eahenle@users.noreply.github.com>

--- src/gram_matrix.jl
+++ src/gram_matrix.jl
@@ -2,39 +2,74 @@ function gram_matrix(
     kernel::Function,
     molecules::Vector{MetaGraph{Int, Float64}};
     normalize::Bool=false,
-    resume_from_error::String="",
+    local_cache::String=".gram_matrix_local_cache",
     kwargs...
 )::Matrix{Float64}
-    if resume_from_error ≠ ""
-        matrix = load(resume_from_error)["matrix"]
-        skip_idx = findall(matrix .≠ -1)
-    else
-        matrix = -1 * ones(length(molecules), length(molecules))
-        skip_idx = []
+    # determine list of jobs
+    ordered_idx = sortperm(nv.(molecules); rev=true)
+    jobs = [
+        (i, j, molecules[i], molecules[j]) for j in ordered_idx for
+        i in ordered_idx if j ≥ i
+    ]
+    # check for local cache
+    if isfile(local_cache)
+        @warn "Using local cache"
+        # check for completed jobs in local cache file
+        skip_jobs = falses(length(jobs))
+        for job in read_local_cache(local_cache)
+            # if found, add to list of jobs to skip
+            job_idx = findfirst(j -> j[1] == job[1] && j[2] == job[2], jobs)
+            skip_jobs[job_idx] = true
+        end
+        # filter job list for only un-completed jobs
+        jobs = jobs[.!skip_jobs]
     end
-    sp = sortperm(nv.(molecules); rev=true)
-    tasks = [(i, j, molecules[i], molecules[j]) for j in sp for i in sp if j ≥ i]
-    filter!(t -> CartesianIndex(t[1], t[2]) ∉ skip_idx, tasks)
+    # the function to run on each job
     f_(x) = x[1], x[2], kernel(x[3], x[4]; kwargs...)
-    k = @showprogress pmap(f_, tasks)
-    for (i, j, k) in k
-        matrix[i, j] = matrix[j, i] = k
+    # results matrix
+    matrix = fill(NaN, length(molecules), length(molecules))
+    # progress meter (but w/o macro, b/c it can squash errors)
+    progress = Progress(length(matrix), 1, "Calculating Gram matrix ")
+    # verify that the matrix is filled completely
+    nans = count(isnan.(matrix))
+    while nans > 0
+        # dispatch jobs to workers
+        @sync @distributed for job in jobs
+            # calculate kernel score
+            (i, j, k) = f_(job)
+            # write to local cache
+            open(local_cache, "a") do f
+                return write(f, "$i,$j,$k\n")
+            end
+        end
+        # collect results from local cache
+        for (i, j, k) in read_local_cache(local_cache)
+            matrix[i, j] = matrix[j, i] = k
+        end
+        # see if any have been missed
+        nans = count(isnan.(matrix))
+        # update progress meter
+        update!(progress, length(matrix) - nans)
     end
-
-    dump_on_error(matrix)
-
+    # (optional) normalize results
     if normalize
         gm_norm!(matrix)
     end
-
+    # clear local cache
+    rm(local_cache)
+    # return results
     return matrix
 end
 
-function dump_on_error(matrix::Matrix{Float64})
-    # running out of RAM can cause values to not be assigned...
-    if any(matrix .== -1)
-        jldsave("gram_matrix_dump_$(split(tempname(), "/")[end]).jld2"; matrix)
-        error("Matrix element(s) not calculated. Something has gone wrong!")
+function read_local_cache(local_cache::String)::Vector{Vector{Int}}
+    # read the cache
+    return open(local_cache, "r") do f
+        # extract the entires from each line in the cache file
+        line_tokens = split.(readlines(f), [","])
+        # if any are incomplete (corrupted cache) drop them
+        filter!(lt -> length(lt) == 3, line_tokens)
+        # parse the i/j/k values
+        return [parse.(Float64, tokens) for tokens in line_tokens]
     end
 end
 
