df89dabf03c347449b2bfdc13cd425b088f071dd
eahenle <eahenle@users.noreply.github.com>
2022-12-20 18:33:57 +0000

    JuliaFormatter Action Bot

--- test/distributed.jl
+++ test/distributed.jl
@@ -51,7 +51,7 @@ using MolecularGraph, MolecularGraphKernels, Test
         open("test_cache", "w") do f
             write(f, "1,1,12\n")
             write(f, "2,2,6\n")
-            write(f, "3,3,14\n")
+            return write(f, "3,3,14\n")
         end
         @test K == gram_matrix(k, graphs; local_cache="test_cache")
     end
